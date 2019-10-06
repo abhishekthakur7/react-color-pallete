5eb36cf95106d5d9f8c19fb6dd9d90cbebaca43e
abhishek <Abhishek Thakur>
2019-10-06 22:53:03 +0530

    Added mini palette component

--- src/components/palettelist/palettelist.component.jsx
+++ src/components/palettelist/palettelist.component.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MiniPalette from "../minipalette/minipalette.component";
 
 export default function PaletteList({ palettes }) {
   return (
     <div>
       <h1>React Colors</h1>
       {palettes.map(palette => (
-        <p>
-          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-        </p>
+        <MiniPalette {...palette} />
       ))}
     </div>
   );
