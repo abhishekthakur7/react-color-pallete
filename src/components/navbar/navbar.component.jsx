5f090fbbe27aefc9bdb2643377cd790f381ce568
abhishek <Abhishek Thakur>
2019-10-06 14:59:17 +0530

    Added select material UI

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import Select from "@material-ui/core/Select";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./navbar.css";
+import MenuItem from "@material-ui/core/MenuItem";
 
-export default function Navbar({ level, handleSliderChange }) {
+export default function Navbar({
+  level,
+  handleSliderChange,
+  handleFormatChange
+}) {
+  const [format, setFormat] = React.useState("hex");
+
+  const handleChange = e => {
+    setFormat(e.target.value);
+    handleFormatChange(e.target.value);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
         <a href="#">ReactColorPicker</a>
       </div>
       <div className="slider-container">
-          <span>Level: {level}</span>
+        <span>Level: {level}</span>
         <div className="slider">
           <Slider
             defaultValue={level}
@@ -20,6 +33,13 @@ export default function Navbar({ level, handleSliderChange }) {
             onAfterChange={handleSliderChange}
           />
         </div>
+      </div>
+      <div className="select-container">
+        <Select value={format} onChange={handleChange}>
+          <MenuItem value="hex">HEX - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb 255,255,255</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba 255,255,255,1.0</MenuItem>
+        </Select>
       </div>
     </nav>
   );
--- src/components/palette/palette.component.jsx
+++ src/components/palette/palette.component.jsx
@@ -5,18 +5,27 @@ import "./palette.css";
 
 export default function Palette({ palette }) {
   const [level, setLevel] = React.useState(500);
+  const [format, setFormat] = React.useState("hex");
 
   const handleSliderChange = level => {
     setLevel(level);
   };
 
+  const handleFormatChange = value => {
+    setFormat(value);
+  };
+
   const colorBoxes = palette.colors[level].map(color => (
-    <Colorbox background={color.hex} name={color.name} />
+    <Colorbox background={color[format]} name={color.name} />
   ));
 
   return (
     <div className="palette">
-      <Navbar level={level} handleSliderChange={handleSliderChange} />
+      <Navbar
+        level={level}
+        handleSliderChange={handleSliderChange}
+        handleFormatChange={handleFormatChange}
+      />
       <div className="palette-colors">{colorBoxes}</div>
       <div>Footer</div>
     </div>
